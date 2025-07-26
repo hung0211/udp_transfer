d8b4a8956867e46044e5d04faaed8da02815e584
hung0211 <nguyenphihung.tech@gmail.com>
2025-07-26 08:29:42 +0700

    config

--- shared/config.py
+++ shared/config.py
@@ -1,4 +1,5 @@
-SERVER_IP = "192.168.1.2"      # cập nhật theo IP bạn đang dùng
+# SERVER_IP = "192.168.1.2" 
+SERVER_IP = "172.16.0.189"    
 SERVER_PORT = 12345
 CHUNK_SIZE = 1024
 TIMEOUT = 3                    # giây
